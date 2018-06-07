a47246819489a53b14de09db4517aa20505b3c80
AndyD9 <andy.dodakal@gmail.com>
2018-06-07 12:05:07 +0200

    Update

---
+++ assets/js/data.js
@@ -0,0 +1,346 @@
+// qst = questions
+// chc = choices
+// stt = stuation
+// dth = death
+// srv = survive
+
+export var data = {
+    stt0: [
+        {
+            index: 0,
+            ref: 'stt-0',
+            content: 'Vous vous réveillez sur une plage de sable chaud. Votre navire a été pris dans une tempête en pleine nuit, et vous vous retrouvez visiblement seul sur cette île.',
+        },
+    ],
+    qst0: [
+        {
+            index: 0,
+            ref : 'qst-0',
+            content: 'Vous êtez fatigué, vous avez faim et soif. Que faites-vous ?',
+        },
+    ],
+    chc1: [
+        {
+            index: 0,
+            ref : 'chc-0',
+            content: '',
+        },
+        {
+            index: 1,
+            ref : 'ch-1',
+            content: 'Chercher des survivants',
+        },
+        {
+            index: 2,
+            ref : 'ch-2',
+            content: 'Chercher des vivres',
+        },
+        {
+            index: 3,
+            ref : 'ch-3',
+            content: 'Chercher un abri',
+        },
+    ],
+    stt1: [
+        {
+            index: 0,
+            ref: 'stt1-0',
+            content: 'Vous faites le tour de l\'île en cherchant de l\'aide. Pour ce faire, vous récoltez quelques pierres pour former un message d\'appel à l\'aide sur la plage.',
+        },
+        {
+            index: 1,
+            ref: 'stt1-1',
+            content: 'Vous décidez de vous enfoncer dans la jungle pour y trouver des baies, des fruits et une source d\'eau potable. Vous récoltez quelques mets qui vous semblent délicieux, et surtout juteux.',
+        },
+        {
+            index: 2,
+            ref: 'stt1-2',
+            content: 'Vous vous enfoncez dans la jungle et récoltez quelques feuillages, ainsi que des branches. En vous promenant , vous découvrez une grotte.',
+        },
+    ],
+    qst1: [
+        {
+            index: 0,
+            ref : 'qst1-0',
+            content: 'Hélas, après avoir silloné la plage un petit moment, vous commencez à être épuisé et n\'avez toujours pas trouvé de survivants. Que faites-vous ?',
+        },
+        {
+            index: 1,
+            ref : 'qst1-1',
+            content: 'Il ne faut pas vous le dire deux fois pour que vous dévoriez votre repas improvisé. Qu\'allez-vous faire ?',
+        },
+        {
+            index: 2,
+            ref : 'qst1-2',
+            content: 'Vous décidez donc de vous y installer et vous faire une couchette pour la nuit. Qu\'allez-vous faire maintenant ?',
+        },
+    ],
+    chc2: [
+
+        {
+            index: 0,
+            ref : 'ch2-0',
+            content: 'Entrer dans la jungle pour trouver de l\'eau',
+        },
+        {
+            index: 1,
+            ref : 'ch2-1',
+            content: 'Continuer à faire le tour de la plage pour se reposer à l\'ombre',
+        },
+        {
+            index: 2,
+            ref : 'ch2-2',
+            content: 'Vous enfoncer encore pour trouver de l\'eau',
+        },
+    ],
+    stt2: [
+        {
+            index: 0,
+            ref: 'stt2-0.1',
+            content: 'Dans la jungle vous trouvez quelques fruits juteux & de l\'eau récoltée par le creux des feuilles. Cela suffira pour le moment.',
+        },
+        {
+            index: 1,
+            ref: 'stt2-0.2',
+            content: 'En continuant votre chemin, vous appercevez ce qui vous semble être une cabane sur la hauteur d\'une grande butte entourée de cailloux. Ce sont peut-être des survivants !',
+        },
+        {
+            index: 2,
+            ref: 'stt2-1.1',
+            content: 'Au loin, vous appercevez quelque chose de brillant. C\'est une boite de raviolis! Vous vous installez à l\'ombre et vous l\’ouvrez à l\’aide d\'un silex.',
+        },
+        {
+            index: 3,
+            ref: 'stt2-1.2',
+            content: 'Après ce délicieux repas, vous entrez dans la jungle pour en explorer ses alentours. Quelques heures de marche plus tard, vous mettez le pied sur un nid de branche et le sol se dérobe sous vos pieds..',
+        },
+        {
+            index: 4,
+            ref: 'stt2-2',
+            content: 'En vous enfonçant, vous entendez un bruit étrange. Une terrible bête se jette sur vous! Vous vous débatez mais vos forces vous abandonnent peu à peu et vous n\'avez aucune arme pour vous défendre.',
+        },
+        {
+            index: 5,
+            ref: 'stt2-3',
+            content: 'En vous promenant en quête d\’un abri, un gros coup de vent passe entre les feuilles. Le vent est si fort qu\' il fait décrocher une noix de coco juste au dessus de vous.',
+        },
+        {
+            index: 6,
+            ref: 'stt2-4',
+            content: 'Vous commencez à marcher dans la jungle, quand tout à coup un cri d\'oiseau vous effraie, vous faisant trébucher etr tomber la tête la première sur un gros rocher.',
+        },
+        {
+            index: 7,
+            ref: 'stt2-5',
+            content: 'Vous retournez sur la plage, et appercevez un sac qui flotte. Vous vous tâtez à aller le chercher, mais vous avez faim, soif, et la fatigue vous gagne..',
+        },
+    ],
+    qst2: [
+        {
+            index: 0,
+            ref: 'qst2-0',
+            content: 'Vous vous précipitez pour la gravir, et une fois en bas de la butte, le chemin vous parrait quelque peu dangereux. Qu\'allez vous faire ?'
+        },
+        {
+            index: 1,
+            ref: 'qst2-1',
+            content: 'Kess ke vous allez faire ?'
+        }
+    ],
+    chc3: [
+        {
+            index: 0,
+            ref: 'ch3-0',
+            content: 'Trouver un autre chemin'
+        },
+        {
+            index: 1,
+            ref: 'chc3-1',
+            content: 'Monter quand même sur les cailloux'
+        },
+        {
+            index: 2,
+            ref: 'chc3-2',
+            content: 'Nager jusqu\'au sac'
+        },
+    ],
+    stt3: [
+        {
+            index: 0,
+            ref: 'stt3-0.1',
+            content: 'Vous vous éloignez un peu en essayant de faire le tour. C\'est là que vous appercevez une sorte de sentier, que vous empruntez. Le sentier vous mène effectivement à la cabane'
+        },
+        {
+            index: 1,
+            ref: 'stt3-0.2',
+            content: 'Vous découvrez plsuieurs habitations faites de bric & de broc av"c des parcelles de terre cultivées. Une femme sors d\'une des cabanes et s\'empresse de vous rejoindre.'
+        },
+        {
+            index: 2,
+            ref: 'stt3-0.3',
+            content: 'Après uen longue discussion, vous apprenez que plusieurs personnes ont échoué sur cette île il y a plusieurs années, de la même façon que vous. Mais ces personnes ont décidés de rester vivre sur cette île.'
+        },
+        {
+            index: 3,
+            ref: 'stt3-0.4',
+            content: 'Les habitant vous rejoignent et vous invite à boire un thé. Ils vous apprennent que l\'île est située non loin du continent et qu\'ils s\'y rendent de temps à autre pour faire du commerce.'
+        },
+        {
+            index: 4,
+            ref: 'stt3-0.5',
+            content: 'Ils vous proposent de rester vivre la grande avanture avec eux. Vous acceptez avec joie. De toute façon, vous aurez toujours des bateaux pour rentrer si un jour vous changez d\'avis..'
+        },
+        {
+            index: 5,
+            ref: 'stt3-1',
+            content: 'Vous montez, et c\'est seulement à la moitié de votre ascension que les rochers se mettent à se décrocher.'
+        },
+        {
+            index: 6,
+            ref: 'stt3-2',
+            content: 'Vous vous enfoncez dans la jungle et trouvez quelques fruits juteux. Un buisson aux baie d\'un rouge profond attire votre regard. Vous en saississez une poignée et l\'ingurgitez goulûment.'
+        },
+        {
+            index: 7,
+            ref: 'stt3-3',
+            content: 'Vous nagez péniblement jusqu\'au sac et vous vous apprêtez à revenir une fois le sac en main'
+        }
+    ],
+    qst3: [
+        {
+            index: 0,
+            ref: 'qst3-0',
+            content: 'Mais vous croyez appercevoir une falque de sang au loin. Que faites-vous ?'
+        },
+    ],
+    chc4: [
+        {
+            index: 0,
+            ref: 'chc4-0',
+            content: 'Retourner sur la plage'
+        },
+        {
+            index: 1,
+            ref: 'chc4-1',
+            content: 'Nager pour atteindre le sang'
+        },
+        {
+            index: 2,
+            ref: 'chc4-2',
+            content: 'Plonger pour découvrir ce que c\'est'
+        }
+    ],
+    stt4: [
+        {
+            index: 0,
+            ref: 'stt4-0',
+            content: 'Vous nagez jusqu\'à appercevoir une lueur au fond de l\'eau.'
+        },
+        {
+            index: 1,
+            ref: 'stt4-1.1',
+            content: 'Vous prenez votre courage à deux mains et plongez après une grande respiration. Vous vous approchez de plus en plus de la lumière jusqu\'à en être ébloui. Vous êtes si proche que vous pourriez la toucher.'
+        },
+        {
+            index: 2,
+            ref: 'stt4-1.2',
+            content: 'Au même moment, quelque chose vous attrape la main. La panique commence à s\'emparer de vous et vouqs tentez tant bien que mal de remonter à la surface. Vous commencez à manquer d\'oxygène.'
+        },
+        {
+            index: 3,
+            ref: 'stt4-2.1',
+            content: 'Une fois le sac installé sur vos épaules, vous regagnez la plage. Vous êtes vraiment à bout de force et vous vous installez à l\'ombre le temps d\'une sieste'
+        },
+        {
+            index: 4,
+            ref: 'stt4-2.2',
+            content: 'Vous vous réveillez quelques heures et vous commencez à avoir faim et soif. Vous regardez ce que contient ce fameux sac. Vous découvrez un couteau suisse, un briquet, des cigarettes humides, quelques conserves, une écharpe et UN TÉLÉPHONE !'
+        },
+        {
+            index: 5,
+            ref: 'stt4-2.3',
+            content: 'Affamé, vous dégustez rapidement l\'une des boites. Vous allez beaucoup et gardez précieusement le sac à vos côtés. Avant de reprendre la route, vous séchez un maximum le téléphone à l\'aide de l\'écharpe.'
+        },
+        {
+            index: 6,
+            ref: 'stt4-2.4',
+            content: 'Vous rentrez de nouveau dans la jungle en quête d\'un abri. En chemin, vous récoltez quelques fruits que vous placez dans votre sac.'
+        },
+        {
+            index: 7,
+            ref: 'stt4-2.5',
+            content: 'Vous découvrez une grotte dans laquelle vous déposez vos trouvailles. Vous êtes si heureux d\'avoir trouvé ce téléphone que vous en prenez grand soin jusqu\' à ce qu\'il soit bien sec.'
+        },
+        {
+            index: 8,
+            ref: 'stt4-2.6',
+            content: 'La nuit commence déjà à tomber, et il va faire trop sombre pour explorer de nouveau. Vous décidez donc de vous reposer en attendant le levé du jour.'
+        },
+        {
+            index: 9,
+            ref: 'stt4-2.7',
+            content: 'Le jour a levé Votre premier réflexe est d\'allumer le télépone. Par chance, le téléphone s\'allume !! Vous arrivez à appeller à les secours  qui arriveront d\'ici quelques heures.'
+        },
+        {
+            index: 10,
+            ref: 'stt4-2.8',
+            content: 'Par le plus grand des hasards, vous n\'étiez pas si loin du continent! Vous en pleurez de joie, vous rejoignez la plage et récupérez des branches pour faire un feu.'
+        }
+    ],
+    qst4: [
+        {
+            index: 0,
+            ref: 'qst4-0',
+            content: 'Qu\'allez-vous essayer de faire ?'
+        }
+    ],
+    dth: [
+        {
+            index: 0,
+            ref: 'dth-0',
+            content: 'Vous tombez au fond d\'un trou immense dont il est impossible de s\'échapper.. Voter jambe s\'est cassé durant votre chute.. Votre aventure s\'arrête ici.. ☠️'
+        },
+        {
+            index: 1,
+            ref: 'dth-1',
+            content: 'La bête est robuste, et finit par vous faire perdre connaissance.. ☠️'
+        },
+        {
+            index: 2,
+            ref: 'dth-2',
+            content: 'Par malchance, le fruit tombe violemment su rvotre tête et vous ouvre le crâne. Personne ne viendra vous aider.. Vous n\'auriez jamais imaginé mourrir d\'une façon aussi ridicule.. ☠️'
+        },
+        {
+            index: 3, 
+            ref: 'dth-3',
+            content: 'Décidément, vous n\'avez pas de chance. Vous voyez votre sang se répandre sur le sol tandis que vos yeux commencent à se fermer.. ☠️'
+        },
+        {
+            index: 4, 
+            ref: 'dth-4',
+            content: 'Vous êtes emporté dans l\'avalanche de cailloux, vous tombez violemment au sol et une grosse roche vous tombe sur la jambe. Vous vous videz peu à peu de votre sang. Personne vous verra ici.. ☠️'
+        },
+        {
+            index: 5, 
+            ref: 'dth-5',
+            content: 'Rassasié, vous vous installez de nouveau dans votre abri. D\'un coup, vous vous sentez pris de violents crampes d\'estomac et vous finissez par vomir toutes vos tripes. Vous regrettez votre repas instantané.. ☠️'
+        },
+        {
+            index: 6, 
+            ref: 'dth-6',
+            content: 'Malhereusement, vous vous épuisez et l\'oxygène commence à manquer. Vous avez été trop utopiste et regrettez votre choix. Désespéré, vous ouvrez la bouche et laissez l\'eau envahir vos poumons.. ☠️'
+        }
+    ],
+    srv: [
+        {
+            index: 0,
+            ref: 'srv-0',
+            content: 'Vous avez survécu! Merci d\'avoir participé au jeu et n\'hésitez pas à nous renvoyer vos avis! 🏝'
+        },
+        {
+            index: 1,
+            ref: 'srv-1',
+            content: 'Une fois fini, vous vous installez à l\'ombre, dégustant une dernière conserve de la victoire. Dans quelques heures, vous allez appercevoir un hélicopère, et dans quelques heures vous serez sur une terre civilisée !! 🏝'
+        }
+    ]
+}
