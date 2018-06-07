40bae4988624447dc69700e69b08cab182fdd093
AndyD9 <andy.dodakal@gmail.com>
2018-06-07 13:34:05 +0200

    Last Update

---
+++ assets/js/main.js
@@ -0,0 +1,333 @@
+
+// Stockage des données
+
+var data = {
+  statement0: [
+      {
+          index: 0,
+          ref: 'statement-0',
+          content: 'Dans cette histoire vous incarnez un jeune forgeron prometteur du nom de Muramasa, pour devenir le meilleur forgeron du japon et obtenir la grâce du shogun, vous décidez de lancer un défis à votre maître, Masamune, se défis consiste à créer la meilleur lame possible puis de comparer leurs tranchant et leurs âmes afin de devenir le meilleur forgeron de l’ère Edo. De nombreuse saison sont passées chacun dans son atelier créant le travail de toute une vie',
+      },
+  ],
+  ask0: [
+      {
+          index: 0,
+          ref : 'ask-0',
+          content: 'L\'heure est pour vous de confectionner votre lame ! Vous devez faire le choix entre la pureté ou le mal.',
+      },
+  ],
+  answer1: [
+      {
+          index: 0,
+          ref : 'answer-0',
+          content: '',
+      },
+      {
+          index: 1,
+          ref : 'answer-1',
+          content: 'Le bien !',
+      },
+      {
+          index: 2,
+          ref : 'answer-2',
+          content: 'Le Mal !',
+      },
+  ],
+  statement1: [
+      {
+          index: 0,
+          ref: 'statement1-0',
+          content: 'Vous mettez tout votre coeur à l\'oeuvre pour confectionner ce katana, une lame blanche et dont les reflets semblaient remplis de sérénité. Au toucher, elle était chaude et agréable et s’accordait avec douceur aux mains de son porteur ; pour cette raison, vous décidez de l’appeler « La Lame des Tendres Mains ».',
+      },
+      {
+          index: 1,
+          ref: 'statement1-0',
+          content: 'Vous mettez tout votre coeur à l\'oeuvre pour confectionner ce katana, une lame sombre, qui brillait d’un éclat majestueux. Elle était froide et langoureuse comme une multitude d’hivers, c’est pourquoi vous décidez de l’appeler « La Lame des Dix Milles Hivers ».',
+      },
+  ],
+  ask1: [
+      {
+          index: 0,
+          ref : 'ask1-0',
+          content: 'Chacun avec son œuvre, maître et disciple se rendirent au bord d’une rivière pour voir laquelle de ces deux armes serait la meilleure. Il s’agissait pour chacun, à tour de rôle, de plonger sa création dans l’onde, tranchant orienté vers l’amont.',
+      },
+  ],
+  answer2: [
+       {
+          index: 0,
+          ref : 'answer2-0',
+          content: 'Vous y aller en premier !',
+       },
+      {
+          index: 1,
+          ref : 'answer2-1',
+          content: 'Vous y aller en premier !',
+      },
+      {
+          index: 2,
+          ref : 'answer2-2',
+          content: 'Vous laissez votre maître y aller !',
+      },
+  ],
+  statement2: [
+      {
+          index: 0,
+          ref: 'statement2-0',
+          content: 'Vous vous engagez en premier, vous placez votre lame dans l’eau, la lame démontra alors son fil sans pareil, tranchant d’abord une feuille morte emportée par le courant, puis tous les poissons qui s’approchèrent. Après avoir vu votre lame en action sûr de vous, vous riez d’une manière démoniaque',
+      },
+      {
+          index: 1,
+          ref: 'statement2-1',
+          content: 'Masamune commence à placer sa lame d’un rayonnement exceptionnel dans l’eau, La feuille l\'evita, les poissons la contournent, la rivière poursuivit son cours. La lame ne trancha rien. Satisfait, Masamune retira son sabre',
+      },
+      {
+        index: 2,
+        ref: 'statement2-2',
+        content: 'Tandis que vous vous moquez des piètres qualités de forgeron de votre maitre. Le moine Ariel qui assista à la scène vint alors expliquer la situation',
+    },
+    {
+        index: 3,
+        ref: 'statement2-3',
+        content: 'Ariel : La lame de Maître Muramasa est certes une lame extraordinaire. Son tranchant est sans égal, mais dans sa main c’est une lame féroce et cruelle, une lame mauvaise. En effet, elle ne fait nulle distinction et tranche tout. Feuille, poissons. Elle ne s’arrête jamais. C’est assurément là la lame la plus vicieuse et la plus mauvaise qu’on ait jamais forgée. La lame de Maître Masamune, en revanche, est douce et pure, elle ne tranche pas les choses innocentes et ne fait pas couler le sang. Elle laisse passer ceux à qui elle n’est pas destinée comme elle a laissé passer feuille, poissons. C’est assurément là, de toutes les lames, la meilleure qu’on ait jamais forgée.',
+    },
+  ],
+  
+  ask2: [
+      {
+          index: 0,
+          ref: 'ask2-0',
+          content: 'Qu’elle est votre état d’esprit à ce moment ?',
+      },
+  ],
+  answer3: [
+      {
+          index: 0,
+          ref: 'answer3-0',
+          content: 'Respectueux'
+      },
+      {
+          index: 1,
+          ref: 'answer3-1',
+          content: 'Furieux'
+      },
+  ],
+  statement3: [
+      {
+          index: 0,
+          ref: 'statement3-0',
+          content: 'Voir une telle lame vous a ouvert les yeux, remplis de respect vous acceptez votre défaite, le moine heureux vous invita dans des montagnes majestueuse afin d’apprendre la bienveillance, et par la suite vous permettre de créer des lames aussi pure que votre maître !'
+      },
+      {
+          index: 1,
+          ref: 'statement3-1',
+          content: 'Furieux d’une telle humiliation vous voulez affronter à mort votre maître, les coups fracassant des armes retentit, laissant apparaître tout le mal de votre sabre et de votre personnalité.',
+      },
+
+  ],
+
+  ask3: [
+    {
+        index: 0,
+        ref: 'ask3-0',
+        content: 'Ou souhaitez vous attaquer masamune ?',
+    },
+],
+  statement4: [
+    {
+        index: 0,
+        ref: 'statement4-0',
+        content: 'Malgré le pouvoir du venin, vous vous êtes affaiblit vous ne tenez plus debout et votre adversaire finit par vous achevée. '
+    },
+    {
+        index: 1,
+        ref: 'statement4-1',
+        content: 'Vous avez gagné - voie du bien',
+    },
+
+],
+
+answer4: [
+    {
+        index: 0,
+        ref: 'answer4-0',
+        content: 'Haut du corps'
+    },
+    {
+        index: 1,
+        ref: 'answer4-1',
+        content: 'Bas du corps'
+    },
+],
+
+statement5: [
+    {
+        index: 0,
+        ref: 'statement5-0',
+        content: 'Vous attaquer masamune vers le haut du corp, malgré la puissance dans ce coup, votre maitre pare et vous assène un coup fatal'
+    },
+    {
+        index: 1,
+        ref: 'statement5-1',
+        content: 'Vous attaquer masamune vers le bas du corps, heureusement pour vous, masamune est impuissant au combat, après lui avoir asséné une seul égratignure, votre lame maléfique s’empara de la vie de votre maitre d’une manière horrible, vous voila maudit….',
+    },
+
+],
+
+
+}
+// Déclaration des varaibale
+var content = document.querySelector('.boxCont');
+var cttClass = 'boxCont ';
+var next = document.querySelector('.btnContNext');
+var choice1 = document.querySelector('.choose1');
+var choice2 = document.querySelector('.choose2');
+var apassa = document.querySelector('.storyBox_choices_apassa');
+var winnez = document.querySelector('.storyBox_choices_winnez');
+
+// Fonction utile
+function init() {
+    content.innerHTML = data.statement0[0].content;
+    next.style.display = 'flex';
+    apassa.style.display = 'none';
+    winnez.style.display = 'none';
+    choice1.innerHTML = data.answer1[1].content;
+    choice2.innerHTML = data.answer1[2].content;
+} 
+function HideUserRep() {
+    choice1.style.display = 'none';
+    choice2.style.display = 'none';
+    next.style.display = 'flex';
+}
+function NotHideUserRep() {
+    choice1.style.display = 'flex';
+    choice2.style.display = 'flex';
+    next.style.display = 'none';
+}
+function apassaUser() {
+    next.style.display = 'none';
+    apassa.style.display = 'flex';
+}
+function youAreTheBoss() {
+    next.style.display = 'none';
+    winnez.style.display = 'flex';
+}
+// On init les fonctions 
+init();
+HideUserRep();
+
+// Ecoute des évnèement
+
+next.addEventListener('click', function() {
+    // Debut
+    if (content.classList.contains(data.statement0[0].ref)) {
+        content.innerHTML = data.ask0[0].content;
+        content.className = cttClass + data.ask0[0].ref;
+        NotHideUserRep();
+    }
+    // Etape 1
+    else if (content.classList.contains(data.statement1[0].ref)) {
+        content.innerHTML = data.ask1[0].content;
+        content.className = cttClass + data.ask1[0].ref;
+        choice1.innerHTML = data.answer2[1].content;
+        choice2.innerHTML = data.answer2[2].content;
+        NotHideUserRep();
+    }
+    // Etape 2
+    else if (content.classList.contains(data.statement2[0].ref)) {
+        content.innerHTML = data.statement2[2].content;
+        content.className = cttClass + data.statement2[2].ref;
+    }
+    // Etape 2-0
+    else if (content.classList.contains(data.statement2[1].ref)) {
+        content.innerHTML = data.statement2[2].content;
+        content.className = cttClass + data.statement2[2].ref;
+    }
+    else if (content.classList.contains(data.statement2[2].ref)) {
+        content.innerHTML = data.statement2[3].content;
+        content.className = cttClass + data.statement2[3].ref;
+    }
+    else if (content.classList.contains(data.statement2[3].ref)) {
+        content.innerHTML = data.ask2[0].content;
+        content.className = cttClass + data.ask2[0].ref;
+        choice1.innerHTML = data.answer3[0].content;
+        choice2.innerHTML = data.answer3[1].content;
+        NotHideUserRep();
+    }
+    else if (content.classList.contains(data.statement3[0].ref)) {
+        content.innerHTML = data.statement4[1].content;
+        content.className = cttClass + data.statement4[1].ref;
+     
+    }
+    else if (content.classList.contains(data.statement3[1].ref)) {
+        content.innerHTML = data.ask3[0].content;
+        content.className = cttClass + data.ask3[0].ref;
+        choice1.innerHTML = data.answer4[0].content;
+        choice2.innerHTML = data.answer4[1].content;
+        NotHideUserRep();
+    }
+    else if (content.classList.contains(data.statement3[1].ref)) {
+        content.innerHTML = data.ask3[0].content;
+        content.className = cttClass + data.ask3[0].ref;
+        choice1.innerHTML = data.answer4[0].content;
+        choice2.innerHTML = data.answer4[1].content;
+        NotHideUserRep();
+    }
+    
+    // Etape 3
+    else {
+        apassaUser();
+    }
+});
+choice1.addEventListener('click', function() {
+    if (content.classList.contains(data.ask0[0].ref)) {
+        content.innerHTML = data.statement1[0].content;
+        content.className = cttClass + data.statement1[0].ref;
+        HideUserRep();
+    }
+    else if (content.classList.contains(data.ask1[0].ref)) {
+        content.innerHTML = data.statement2[0].content;
+        content.className = cttClass + data.statement2[0].ref;
+        HideUserRep();
+    }
+    else if (content.classList.contains(data.ask2[0].ref)) {
+        content.innerHTML = data.statement3[0].content;
+        content.className = cttClass + data.statement3[0].ref;
+        HideUserRep();
+    }
+    else if (content.classList.contains(data.ask3[0].ref)) {
+        content.innerHTML = data.statement5[0].content;
+        content.className = cttClass + data.statement5[0].ref;
+        HideUserRep();
+    }
+    else {
+        content.innerHTML = 'WTF bro';
+    }
+});
+choice2.addEventListener('click', function() {
+    if (content.classList.contains(data.ask0[0].ref)) {
+        content.innerHTML = data.statement1[1].ref + 
+        ' _ ' + data.statement1[1].content;
+        content.className = cttClass + data.statement1[1].ref;
+        HideUserRep();
+    }
+    else if (content.classList.contains(data.ask1[0].ref)) {
+        content.innerHTML = data.statement2[1].content;
+        content.className = cttClass + data.statement2[1].ref;
+        HideUserRep();
+    }
+
+    else if (content.classList.contains(data.ask2[0].ref)) {
+        content.innerHTML = data.statement3[1].content;
+        content.className = cttClass + data.statement3[1].ref;
+        HideUserRep();
+    }
+    else if (content.classList.contains(data.ask3[0].ref)) {
+        content.innerHTML = data.statement5[1].content;
+        content.className = cttClass + data.statement5[1].ref;
+        HideUserRep();
+    }
+    else {
+        content.innerHTML = 'ERROR';
+    }
+});
+
